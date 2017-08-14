3dfc0b6182283543a874a8238bb9701483f7803d
hometownzero <bradycpeters@gmail.com>
2017-08-14 16:00:10 -0500

    tested email, it works now subbing in the real values

--- edssite/main.py
+++ edssite/main.py
@@ -52,8 +52,8 @@ class FrontpageHandler(Handler):
         number = self.request.get("number")
         body   = self.request.get("body")
 
-        mail.send_mail(sender = "bradycpeters@gmail.com",
-                        to = "Brady Peters <bradycpeters@gmail.com>",
+        mail.send_mail(sender = "fitnessstl@gmail.com",
+                        to = "<fitnessstl@gmail.com>",
                         subject = "business inquiry",
                         body = "Name: {} \n Email: {} \n Contact number: {} \n Message: {}".format(name,email,number,body))
 
